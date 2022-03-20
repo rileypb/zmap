c5833dd9cb037555a14731b8179a3772fab29e54
Phil Riley <rileypb@gmail.com>
2022-03-20 11:28:31 -0400

    refactor main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -13,78 +13,80 @@ from PyQt5.QtCore import (
 from PyQt5 import uic
 from compiler import Compiler
 
-settings = None
-scene = None
-win = None
-zmap_compiler = Compiler()
 
-current_filename = None
+class ZApp:
+    def __init__(self):
+        self.appctxt = ApplicationContext()
+        self.current_filename = None
+        self.zmap_compiler = Compiler()
 
-def compile(*args):
-    scene.clear()
-    map_names = zmap_compiler.compile(win.plainTextEdit.toPlainText())
-    current_rendered_map = win.graphChooser.currentText()
-    win.graphChooser.clear()
-    win.graphChooser.addItems(map_names)
-    if current_rendered_map in map_names:
-        win.graphChooser.setCurrentIndex(map_names.index(current_rendered_map))
+    def setup(self):
+        self.win = QMainWindow()
+        self.win.setWindowTitle("zmap")
+        self.win.show()
 
-def display(*args):    
-    scene.clear()
-    zmap_compiler.display(win.graphChooser.currentText(), scene)        
+        self.settings = QSettings("Brainfreeze", "zmap")
 
-def save_zmap(*args):
-    if current_filename:
-        with open(current_filename, 'w') as word_file:
-            word_file.write(win.plainTextEdit.toPlainText())
-    else:
-        filedir = settings.value("file/dir", os.path.expanduser("~"))
+        rsrc = self.appctxt.get_resource('zmap.ui')
+        uic.loadUi(rsrc, self.win)
+
+        self.win.splitter.setSizes([200, 400])
+
+        self.scene = QGraphicsScene()
+        self.win.graphicsView.setScene(self.scene)
+        self.win.actionCompile.triggered.connect(self.compile)
+        self.win.actionSave.triggered.connect(self.save_zmap)
+        self.win.actionOpen.triggered.connect(self.open_zmap)
+
+        self.win.graphChooser.currentIndexChanged.connect(self.display)
+
+    def run(self):
+        exit_code = self.appctxt.app.exec()      # 2. Invoke appctxt.app.exec()
+        self.settings.sync()
+        sys.exit(exit_code)
+
+    def compile(self, *args):
+        self.scene.clear()
+        map_names = self.zmap_compiler.compile(self.win.plainTextEdit.toPlainText())
+        current_rendered_map = self.win.graphChooser.currentText()
+        self.win.graphChooser.clear()
+        self.win.graphChooser.addItems(map_names)
+        if current_rendered_map in map_names:
+            self.win.graphChooser.setCurrentIndex(map_names.index(current_rendered_map))
+
+    def display(self, *args):    
+        self.scene.clear()
+        self.zmap_compiler.display(self.win.graphChooser.currentText(), self.scene)        
+
+    def save_zmap(self, *args):
+        if self.current_filename:
+            with open(self.current_filename, 'w') as word_file:
+                word_file.write(self.win.plainTextEdit.toPlainText())
+        else:
+            filedir = self.settings.value("file/dir", os.path.expanduser("~"))
+            options = QFileDialog.Options()
+            filename, _ = QFileDialog.getSaveFileName(self.win, "Saving zmap file", filedir, "zmap Files (*.zmap);;All Files (*)", options=options)
+            if filename:
+                dirname = os.path.dirname(filename)
+                self.settings.setValue("file/dir", dirname)
+                with open(filename, 'w') as word_file:
+                    word_file.write(self.win.plainTextEdit.toPlainText())
+
+    def open_zmap(self, *args):
+        filedir = self.settings.value("file/dir", os.path.expanduser("~"))
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(win, "Saving zmap file", filedir, "zmap Files (*.zmap);;All Files (*)", options=options)
+        filename, _ = QFileDialog.getOpenFileName(self.win, "Opening zmap file", filedir, "zmap Files (*.zmap);;All Files (*)", options=options)
         if filename:
             dirname = os.path.dirname(filename)
-            settings.setValue("file/dir", dirname)
-            with open(filename, 'w') as word_file:
-                word_file.write(win.plainTextEdit.toPlainText())
-
-def open_zmap(*args):
-    global current_filename
-    filedir = settings.value("file/dir", os.path.expanduser("~"))
-    options = QFileDialog.Options()
-    filename, _ = QFileDialog.getOpenFileName(win, "Opening zmap file", filedir, "zmap Files (*.zmap);;All Files (*)", options=options)
-    if filename:
-        dirname = os.path.dirname(filename)
-        settings.setValue("file/dir", dirname)
-        with open(filename, 'r') as word_file:
-            mapstring = word_file.read()
-            win.plainTextEdit.setPlainText(mapstring)
-            compile()
-            current_filename = filename
+            self.settings.setValue("file/dir", dirname)
+            with open(filename, 'r') as word_file:
+                mapstring = word_file.read()
+                self.win.plainTextEdit.setPlainText(mapstring)
+                self.compile()
+                self.current_filename = filename
 
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    win = QMainWindow()
-    win.setWindowTitle("zmap")
-    win.show()
-
-    rsrc = appctxt.get_resource('zmap.ui')
-    print(rsrc)
-
-    settings = QSettings("Brainfreeze", "zmap")
-    
-    uic.loadUi(rsrc, win)
-
-    win.splitter.setSizes([200, 400])
-
-    scene = QGraphicsScene()
-    win.graphicsView.setScene(scene)
-    win.actionCompile.triggered.connect(compile)
-    win.actionSave.triggered.connect(save_zmap)
-    win.actionOpen.triggered.connect(open_zmap)
-
-    win.graphChooser.currentIndexChanged.connect(display)
-
-    exit_code = appctxt.app.exec()      # 2. Invoke appctxt.app.exec()
-    settings.sync()
-    sys.exit(exit_code)
+    zapp = ZApp()
+    zapp.setup()
+    zapp.run()
